8d466df3acec6f3bf5d1f59333eb51eb9747f4bd
jsgr37920 <jsgr37920@edu.ucl.dk>
2022-05-20 12:12:18 +0200

    Styleguide

---
+++ Styleguide/styleguidep6.js
@@ -0,0 +1,9 @@
+
+
+const copyBtn = document.getElementById('copyBtn')
+const copyText = document.getElementById('copyText')
+
+copyBtn.onclick = () => {
+  copyText.select();
+  document.execCommand('copy');
+}
