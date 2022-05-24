7be0df6b92afde43c2c8730c0e313eb3f9bd626e
chpe38426 <chpe38426@edu.ucl.dk>
2022-05-24 20:38:19 +0200

    Update på javascript

--- scripts/tjenester.js
+++ scripts/tjenester.js
@@ -1,9 +1,9 @@
-var image_input = document.querySelector("#image-input");
+let image_input = document.querySelector("#image-input");
 
 image_input.addEventListener("change", function() {
-  var reader = new FileReader();
+  let reader = new FileReader();
   reader.addEventListener("load", () => {
-    var uploaded_image = reader.result;
+    let uploaded_image = reader.result;
     document.querySelector(".contentforbox").style.backgroundImage = `url(${uploaded_image})`;
   });
   reader.readAsDataURL(this.files[0]);
@@ -11,20 +11,33 @@ image_input.addEventListener("change", function() {
 
 // Denne kode oppe i toppen er til billedevalg.
 
+// Denne kode oppe i bunden er til form.
 
-let email = document.getElementById('inputemail')
+let email = document.getElementById('email')
 let password = document.getElementById('password')
 let form = document.getElementById('form')
 let errorElement = document.getElementById('error')
 
-form.addEventListener('submit', (e) =>{
+form.addEventListener('submit', (e) => {
   let messages = []
-  if (name.value === '' || name.value == null) {
-    messages.push('Email is required')
+  if (email.value === '' || email.value == null) {
+    messages.push('Name is required')
+  }
+
+  if (password.value.length <= 6) {
+    messages.push('Password must be longer than 6 characters')
+  }
+
+  else {
+  messages.push('Password is really strong!')
+}
+
+  if (password.value === 'password') {
+    messages.push('Password cannot be password')
   }
 
   if (messages.length > 0) {
-e.preventdefault()
-errorElement.innerText = messages.join(', ')
-}
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
 })
