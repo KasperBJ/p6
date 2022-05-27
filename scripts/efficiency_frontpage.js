0e99fe8a72e299cc073fda640847b9ae9e309881
ischmir <timmhinsch@gmail.com>
2022-05-27 22:08:21 +0200

    pop up menuer
    log ud og notifikations pop up virker

--- scripts/efficiency_frontpage.js
+++ scripts/efficiency_frontpage.js
@@ -1,1 +1,31 @@
+/* Javascript by Timm Hinsch */
+
+/* Test */
 console.log('js running')
+
+/* Startside js */
+
+  /* notifikations knappen */
+  function showNtfy() {
+    let x = document.getElementById("notifications");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+/* log ud knappen */
+function showLogout() {
+    let x = document.getElementById("logoutButton");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+  /* læs mere pomodoro */
+
+  /* læs mere organize */
+
