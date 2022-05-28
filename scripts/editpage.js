ca35741d89881def6578d93a29349c9b81e5cd24
chpe38426 <chpe38426@edu.ucl.dk>
2022-05-28 14:47:53 +0200

    Flere divs

--- scripts/editpage.js
+++ scripts/editpage.js
@@ -1,17 +1,9 @@
 function myFunction() {
   let x = document.getElementById("myDIV");
+
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
-    x.style.display = "none";
-  }
-}
-
-function myFunctiontwo() {
-  var x = document.getElementById("myDIVtwo");
-  if (x.style.display === "none")
-  { x.style.display = "block";}
-  else {
     x.style.display = "none";
   }
 }
