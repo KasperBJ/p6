1e02dfba71bbf2201d97e8d583fb837da167d92c
ischmir <timmhinsch@gmail.com>
2022-05-28 12:25:06 +0200

    desktop startside done

--- scripts/efficiency_frontpage.js
+++ scripts/efficiency_frontpage.js
@@ -6,6 +6,7 @@ console.log('js running')
 /* Startside js */
 
   /* notifikations knappen */
+  document.getElementById("notifications").style.display = "none";
   function showNtfy() {
     let x = document.getElementById("notifications");
     if (x.style.display === "none") {
@@ -16,6 +17,7 @@ console.log('js running')
 }
 
 /* log ud knappen */
+document.getElementById("logoutButton").style.display = "none";
 function showLogout() {
     let x = document.getElementById("logoutButton");
     if (x.style.display === "none") {
@@ -26,6 +28,7 @@ function showLogout() {
 }
 
   /* læs mere pomodoro */
+  document.getElementById("startside-about-pomodoro").style.display = "none";
   function ReadMorePomodoro() {
     let x = document.getElementById("startside-about-pomodoro");
     if (x.style.display === "none") {
@@ -36,6 +39,7 @@ function showLogout() {
 }
 
   /* læs mere organize */
+  document.getElementById("startside-about-organize").style.display = "none";
   function ReadMoreOrganize() {
     let x = document.getElementById("startside-about-organize");
     if (x.style.display === "none") {
